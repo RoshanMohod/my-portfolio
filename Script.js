0347ce8f7b1a917a13d883cf5a9f2dfae14a4088
RoshanMohod <roshanmohod.tech@gmail.com>
2026-07-19 10:44:09 +0530

    add mobile view

--- Script.js
+++ Script.js
@@ -53,3 +53,9 @@ function typeEffect() {
 }
 
 typeEffect();
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
